47a20d1478ae27c649cb2df3474a886bf4f65915
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-11-07 07:04:46 +0700

    update migration

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,10 +13,10 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('quiz', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
+            $table->int('author');
+            $table->string('connection');
             $table->text('queue');
             $table->longText('payload');
             $table->longText('exception');
