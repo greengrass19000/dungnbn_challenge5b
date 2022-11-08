17f6ec6b8e9b4cf8fa58653513c998db33d111a0
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2022-11-08 10:15:24 +0700

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,7 @@
-<div id="page-content">
-    <div class="container text-center">
-      <div class="row justify-content-center">
-        <div class="col-md-7">
-          <h1 class="fw-light mt-4 text-white">Sticky Footer using Flexbox</h1>
-          <p class="lead text-white-50">Use just two Bootstrap utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
-        </div>
+<div class="d-flex flex-column h-100">    
+    <footer class="footer mt-auto py-3 bg-dark">
+      <div class="container">
+        <span class="text-light">Challenge5b_dungnbn</span>
       </div>
-    </div>
-  </div>
-  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
-    <div class="container text-center">
-      <small>Copyright &copy; Challenge5b_dungnbn</small>
-    </div>
-  </footer>
+    </footer>
+</div>
